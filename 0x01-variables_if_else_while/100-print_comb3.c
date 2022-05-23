16fddf07e5897d89722962fed9cc850b29f6b04d
EthanWakeford <ethanwakeford@gmail.com>
2022-05-23 13:02:39 -0700

    'added 100'

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int x;
+	
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (x = '0'; x <= '9'; x++)
+		{
+			if (n < x)
+			{
+				putchar(n);
+				putchar(x);
+				if (n < '8')
+				{
+					putchar(',');
+					putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
