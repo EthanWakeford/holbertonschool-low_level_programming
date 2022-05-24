a6045b8cd7a18721f0698de7e5c898648f4e4ad6
EthanWakeford <ethanwakeford@gmail.com>
2022-05-24 11:32:06 -0700

    'added 5'

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*print_sign - prints the sign of the number
+*
+*@n: number to be checked
+*Return: 1 if positive, 0 if zero, -1 if negative
+*/
+
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
