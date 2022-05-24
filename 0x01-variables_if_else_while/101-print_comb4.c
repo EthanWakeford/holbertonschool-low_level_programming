5b0d9e738357d983ef0c6d06399a623983891175
EthanWakeford <ethanwakeford@gmail.com>
2022-05-23 17:10:02 -0700

    'added 101'

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+*main - print possible 3 digit combinations
+*
+*Return: none
+*/
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = '0'; x <= '7'; x++)
+	{
+		for (y = '0'; y <= '8'; y++)
+		{
+			for (z = '0'; z <= '9'; z++)
+			{
+				if (x < y && y < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x < '7')
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
