9b9adac03adde8f49d0328999c9f56b7ba87c777
EthanWakeford <ethanwakeford@gmail.com>
2022-05-27 08:49:26 -0700

    'formatted indents'

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else if (c > a && c > b)
-    {
-        largest = c;
-    }
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else if (c > a && c > b)
+	{
+		largest = c;
+	}
 	else if (a == b || b == c)
 		{
 		largest = b;
@@ -32,5 +32,5 @@ int largest_number(int a, int b, int c)
 		{
 		largest = a;
 		}
-    return (largest);
+	return (largest);
 }
