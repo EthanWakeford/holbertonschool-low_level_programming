726727ab5acd24d234aa2afb1bdf3f4f97322c2a
EthanWakeford <ethanwakeford@gmail.com>
2022-05-31 08:24:51 -0700

    'updated 9'

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,15 +7,13 @@
 void main(void)
 {
 	int x;
-	char fizz[] = "Fizz";
-	char buzz[] = "Buzz";
 
 	for (x = 1; x <= 100; x++)
 		{
 		if (x % 3 == 0)
-			printf("%s", fizz);
+			printf("Fizz");
 		if (x % 5 == 0)
-			printf("%s", buzz);
+			printf("Buzz");
 		if ((x % 3 != 0) && (x % 5 != 0))
 			printf("%d", x);
 		if (x < 100)
