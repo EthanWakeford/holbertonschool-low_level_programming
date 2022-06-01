044bc6b7ebcb98f18e21bdbe0397fa98e9363615
EthanWakeford <ethanwakeford@gmail.com>
2022-06-01 09:05:10 -0700

    'updated 1'

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+*swap_int - swa[s the value of 2 integers
+*
+*@a: two one to be swapped
+*@b: int 2
+*/
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
