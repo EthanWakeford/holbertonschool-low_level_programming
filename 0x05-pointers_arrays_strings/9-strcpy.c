76106fd4667957d7214d3e0e76dc1986748fb718
EthanWakeford <ethanwakeford@gmail.com>
2022-06-02 08:10:59 -0700

    'added 9'

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+**_strcpy - copies string to dest from src
+*@dest: destination string
+*@src: source string
+*Return: the pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	char *p = dest;
+
+	*p = src;
+	dest = src;
+	return (dest);
+}
