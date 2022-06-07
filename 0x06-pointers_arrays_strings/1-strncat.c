71bad6c7e15779e9463bb8e2e5eaf36f9a72102d
EthanWakeford <ethanwakeford@gmail.com>
2022-06-07 14:56:50 -0700

    'updated 1 for betty'

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,20 @@
- char *_strncat(char *dest, char *src, int n)
+#include "holberton.h"
+
+/**
+*_strncat - concatenates one string to another to the nth element
+*@dest: string to be added onto
+*@src: string to be concatenated
+*return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int s = 1;
 
 	while (dest[i] != '\0')
 	{
-	    i++;
+	i++;
 	}
 	dest[i] = src[0];
 	i++;
@@ -15,4 +24,4 @@
 		i++;
 	}
 	return (dest);
- }
+}
