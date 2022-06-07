737af53fa77b7444741dace01e274ca847408b44
EthanWakeford <ethanwakeford@gmail.com>
2022-06-07 12:36:50 -0700

    'renamed header, added 0'

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int s = 1;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	dest[i] = src[0];
+	i++;
+	for (s = s; src[s] != '\0'; s++)
+	{
+		dest[i] = src[s];
+		i++;
+	}
+	i++;
+	dest[i] = src[s];
+	return(dest);
+}
