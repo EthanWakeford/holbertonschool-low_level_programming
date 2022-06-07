d918ebe72de86b98f59bec729ae5e9279fac9880
EthanWakeford <ethanwakeford@gmail.com>
2022-06-07 15:59:45 -0700

    'updated 4'

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,14 @@
 void reverse_array(int *a, int n)
 {
 	int i, tmp, mid;
-
+	
+	n = n-1;
 	mid = n / 2;
 	for (i = 0; i <= mid; i++)
 	{
 		tmp = a[i];
-		a[i] = a[n - 1];
-		a[n - 1] = tmp;
+		a[i] = a[n];
+		a[n] = tmp;
 		n--;
 	}
 }
