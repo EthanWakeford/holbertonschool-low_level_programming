3d2a1c0063b7839c20fc5727d0cb0d31e1253c19
EthanWakeford <ethanwakeford@gmail.com>
2022-06-07 15:14:13 -0700

    added 2 and updated 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,17 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int s = 1;
+	int len = 0;
+	int i;
 
-	while (dest[i] != '\0')
+	while (dest[len] != '\0')
 	{
-	i++;
+		len++;
 	}
-	dest[i] = src[0];
-	i++;
-	for (s = s; (s < n) && (src[s] != '\0'); s++)
+	for (i = 0; (i < n) && (src[i] != '\0'); i++)
 	{
-		dest[i] = src[s];
-		i++;
+		dest[len + i] = src[i];
 	}
+	dest[len + i] = '\0';
 	return (dest);
 }
