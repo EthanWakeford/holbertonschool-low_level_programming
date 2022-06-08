e0753d2ab5e81185a74b0757c59b3d15c9d6ba93
EthanWakeford <ethanwakeford@gmail.com>
2022-06-07 18:03:25 -0700

    'updated 3'

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,15 +4,18 @@
 *_strcmp - compares 2 strings
 *@s1: string 1 to compare
 *@s2: string 2 to compare
-*Return: 15 if s1 > s2, -15 if s1 < s2, 0 otherwise
+*Return: difference
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
-		return (15);
-	else if (s2 > s1)
-		return (-15);
-	else
-		return (0);
+	int i;
+
+	while ((s1 != '\0' && s2 != '\0') && s1 == s2)
+	{
+		s1++
+		s2++
+	}
+	i = s1 - s2
+	return (i);
 }
