dcff465480f699d9b5c6f6cdbc1752639dabdf98
EthanWakeford <ethanwakeford@gmail.com>
2022-06-07 18:04:20 -0700

    'updated 3'

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	while ((s1 != '\0' && s2 != '\0') && s1 == s2)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++
 		s2++
 	}
-	i = s1 - s2
+	i = *s1 - *s2
 	return (i);
 }
