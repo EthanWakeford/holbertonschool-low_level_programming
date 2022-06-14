8a04b2ae78c4f1dd4e722ea2e0abb4ee7ff048c8
EthanWakeford <ethanwakeford@gmail.com>
2022-06-14 15:03:16 -0700

    'added 4'

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+*main - adds args together
+*@argc: num of args
+*@argv: pointer to array of args
+*Return: 0 if successful, 1 if not
+int main(int argc, char *argv[])
+{
+	int i, j, num, num2;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (num = 1; num < argc; num++)
+		{
+			for (num2 = 0; argv[num][num2] != '\0'; num2++)
+			{
+				if (argv[num][num2] > 57 || argv[num][num2] < 48)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+		i = atoi(argv[1]);
+		for	(num = 2; num < argc; num++)
+		{
+			j = atoi(argv[num]);
+			i = i + j;
+		}
+		printf("%d\n", i);
+	}
+	return (0);
+}
