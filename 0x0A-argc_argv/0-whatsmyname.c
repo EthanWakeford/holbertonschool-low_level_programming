a98e4c3141a9b83f248870164f544c164ca4fe71
EthanWakeford <ethanwakeford@gmail.com>
2022-06-14 13:56:38 -0700

    'updated for betty'

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
-int main (int argc, char *argv[])
+/**
+*main - prints out name of file
+*@argc: num of command line arguments
+*@argv: pointer to array of arguments
+*Return: always 0
+*/
+
+int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	(void)argc;
 	for (i = 0; argv[0][i] != '\0'; i++)
 	{
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+*main - prints the number of command line arguments
+*@argc: num of arguments
+*@argv: pointer to array of arguments
+*Return: always zero
+*/
+
 int main(int argc, char *argv[])
 {
 	(void)argv;
 	printf("%d\n", --argc);
-	return(0);
+	return (0);
 }
