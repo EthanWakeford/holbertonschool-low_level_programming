b810b56c4533565d9f8f48c64beed79de7695e79
EthanWakeford <ethanwakeford@gmail.com>
2022-06-14 14:03:06 -0700

    'adde 2'

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main - prints all arguments it receives
+*@argc: num of arguments
+*@argv: pointer to array of args
+*Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	while(argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
