4dc617c09e873a1161312d970caf22daa3e82aaa
EthanWakeford <ethanwakeford@gmail.com>
2022-06-16 14:32:27 -0700

    'updated one to pass linter'

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+*_strdup - returns pointer to newly allocated space, which is copy of str
+*@str: string to copy
+*Return: NUll if unsucessful, ptr otherwise
+*/
+
 char *_strdup(char *str)
 {
 	char *ptr;
@@ -12,12 +18,12 @@ char *_strdup(char *str)
 	{
 		len++;
 	}
-	ptr = (char *)malloc(len * sizeof(char));
+	ptr = (char *)malloc((len * sizeof(char)) + 1);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
 	{
 		ptr[i] = str[i];
 	}
-	return(ptr);
+	return (ptr);
 }
