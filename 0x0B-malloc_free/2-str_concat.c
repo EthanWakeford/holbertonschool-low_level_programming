4757bfa71df2b314c626c383bbb22032032decf5
EthanWakeford <ethanwakeford@gmail.com>
2022-06-16 14:51:55 -0700

    'added 2'

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+*lengthof - gets length of string
+*@str: string
+*Return: len
+*/
+
+int lengthof(char *str)
+{
+	int len = 1;
+
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+*str_concat - concats 2 strings
+*@s1: string 1
+*@s2: string 2
+*Return: NUll if failed, ptr if passed
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int len1, len2, i;
+
+	len1 = lengthof(s1);
+	len2 = lengthof(s2);
+	ptr = (char *)malloc((len1 * sizeof(char)) + (len2 * sizeof(char)) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (i = i; i < (len1 + len2); i++)
+	{
+		ptr[i] = s2[i - len1];
+	}
+	return (ptr);
+}
