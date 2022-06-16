e3c00107ff41cfb041bcbc966fa7ffd21210c87d
EthanWakeford <ethanwakeford@gmail.com>
2022-06-16 12:28:21 -0700

    'updated 0'

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ar = (char *)malloc(size);
+	ar = (char *)malloc(size * sizeof(char));
 	for (i = 0; i < 98; i++)
 	{
 		ar[i] = c;
