0c0d2d4531be0a66cd2db0e61ff5f2069ba9700f
EthanWakeford <ethanwakeford@gmail.com>
2022-06-21 10:55:10 -0700

    'updated 2'

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,15 +9,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i;
-	int *ar;
+	unsigned int i;
+	char *ar;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ar = malloc(nmemb * size);
 	if (ar == NULL)
 		return (NULL);
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		ar[i] = 0;
 	}
