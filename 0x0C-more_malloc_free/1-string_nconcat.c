31c5aa8c015b8155fe01a780801661178bdb544a
EthanWakeford <ethanwakeford@gmail.com>
2022-06-21 10:23:12 -0700

    'added 1'

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,6 +4,8 @@ int stringlength(char *str)
 {
 	int i = 0;
 
+	if (str[i] == NULL)
+		return (0);
 	while (str[i] != '\0')
 	{
 		i++;
@@ -30,9 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[i] = s1[i];
 	}
-	for (i = 0; i <= (int)n && s2[i] != '\0'; i++)
+	for (i = 0; i < (int)n && s2[i] != '\0'; i++)
 	{
 		ptr[i + l1] = s2[i];
 	}
+	ptr[i + l1] = '\0';
 	return (ptr);
 }
