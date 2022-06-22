8e5a030de53cf9401c5dd8755fcefee27fcf0c46
EthanWakeford <ethanwakeford@gmail.com>
2022-06-22 14:24:10 -0700

    'updated header'

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdlib.h>
+#include <string.h>
 
 /**
 *struct dog - dog
@@ -18,7 +19,7 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
-/*void free_dog(dog_t *d);*/
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
