e4446591b65859a8d57999f725f1232c82f5e383
EthanWakeford <ethanwakeford@gmail.com>
2022-06-22 12:59:40 -0700

    'added 1'

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
+
 /**
 *struct dog - dog
 *@name: name
@@ -13,5 +15,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/*dog_t *new_dog(char *name, float age, char *owner);*/
+void free_dog(dog_t *d);
 
 #endif
