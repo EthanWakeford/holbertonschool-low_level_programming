d38491855c39e80ae5e29646b80285ccce5f1e4f
EthanWakeford <ethanwakeford@gmail.com>
2022-06-28 10:04:27 -0700

    'added 1 and 2'

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+*print_strings - prints out strings
+*@separator: separator between strings
+*@n: num of optional args
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	va_start(ap, n);
+	str = va_arg(ap, char *);
+	if (str == NULL)
+		printf("(nil)");
+	else
+		printf("%s", str);
+	for (i = 1; i < n; i++)
+	{
+		if (separator != NULL)
+			printf("%s", separator);
+		str = va_arg(ap, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+	}
+	putchar('\n');
+}
