8f9402e233080086de121c3a069cefcabfff8677
EthanWakeford <ethanwakeford@gmail.com>
2022-07-08 10:10:22 -0700

    'added 3'

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,12 +2,28 @@
 
 /**
 *add_node_end - adds a new node at end of linked list
-*@head: asdlf
-*@str: asdf
-*Return: asdf
+*@head: pointer to first element
+*@str: string to add
+*Return: addess of new element
 */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	return (head);
+	list_t *p, *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+	if (*head != NULL)
+	{
+		for (p = *head; p->next != NULL; p = p->next)
+		{}
+		p->next = new;
+	}
+	if (*head == NULL)
+		*head = new;
+	return (new);
 }
