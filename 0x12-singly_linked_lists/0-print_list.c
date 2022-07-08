19ab67efbe31f545e87ae88ab566f63066f64cd6
EthanWakeford <ethanwakeford@gmail.com>
2022-07-08 08:46:52 -0700

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,12 +2,24 @@
 
 /**
 *print_list - prints all elements of list_t list
-*@h: blah
-*Return: the number of nodes
+*@h: first item in list
+*Return: the number of nodes, 0 if fail
 */
 
 size_t print_list(const list_t *h)
 {
-	h = h + 1;
-	return (0);
+	size_t count = 0;
+	const list_t *p = h;
+
+	if (h == NULL)
+		return (0);
+	for (; p != NULL; p = p->next)
+	{
+		if (p->str != NULL)
+			printf("[%d] %s\n", p->len, p->str);
+		else
+			printf("[0] (nil)\n");
+		count++;
+	}
+	return (count);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,11 +2,16 @@
 
 /**
 *list_len - return number of elementss in linked list
-*@h: b
-*Return: ?
+*@h: first item in list
+*Return: count of items in list
 */
 
 size_t list_len(const list_t *h)
 {
-	return (h);
+	size_t count = 0;
+	const list_t *p = h;
+
+	for (; p != NULL; p = p->next)
+		count++;
+	return (count);
 }
