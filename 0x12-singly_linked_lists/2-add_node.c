15f1a74dd091c7cf7344a68577a19a2713315085
EthanWakeford <ethanwakeford@gmail.com>
2022-07-08 09:10:50 -0700

    'added 2'

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,12 +2,21 @@
 
 /**
 *add_node - adds a new node at beginning of linked list
-*@head: adf
-*@str: sdgf
-*Return: ajsdf
+*@head: pointer to first element of list
+*@str: string to duplicate
+*Return: poinbter to new element
 */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	return (head);
+	list_t *p;
+
+	p = malloc(sizeof(list_t));
+	if (p == NULL)
+		return (NULL);
+	p->str = strdup(str);
+	p->len = strlen(str);
+	p->next = *head;
+	*head = p;
+	return (*head);
 }
