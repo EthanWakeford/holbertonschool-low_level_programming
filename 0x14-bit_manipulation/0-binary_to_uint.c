c2ec69800109a33461ebf704ccaec03422397bfb
EthanWakeford <ethanwakeford@gmail.com>
2022-07-15 10:30:15 -0700

    'updated 0'

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,14 +9,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	long bin = 0;
+	long mul = 1;
 	int i;
 
 	if (b == NULL)
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+	i = (strlen(b)) - 1;
+	for (; i >= 0; i--, mul *= 2)
 	{
 		if ((b[i] != '1') & (b[i] != '0'))
 			return (0);
+		bin += mul * (b[i] - 48);
 	}
 	return (bin);
 }
