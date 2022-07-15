26f97cdbf9a0a60e8ed32233e113a3089f25697d
EthanWakeford <ethanwakeford@gmail.com>
2022-07-15 11:08:12 -0700

    'updated 1 with recuriuon'

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,18 +7,9 @@
 
 void print_binary(unsigned long int n)
 {
-	int i;
-	int arr[64];
-
-	if (n == 0)
-		_putchar('0');
-	for (i = 0; n > 0; i++)
+	if (n / 2 != 0)
 	{
-		arr[i] = n % 2;
-		n = n / 2;
+		print_binary(n / 2);
 	}
-	for (i = i - 1; i >= 0; i--)
-	{
-		_putchar(arr[i] + 48);
-	}
+	_putchar((n % 2) + 48);
 }
