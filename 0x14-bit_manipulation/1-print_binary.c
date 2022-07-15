ee1f00173bfb94892f2966631792910e3485bdb8
EthanWakeford <ethanwakeford@gmail.com>
2022-07-15 11:13:14 -0700

    'updated 1'

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,9 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n / 2 != 0)
-	{
-		print_binary(n / 2);
-	}
-	_putchar((n % 2) + 48);
+	if (n > 1)
+		print_binary(n >> 1);
+	printf("%lu", n & 1);
 }
