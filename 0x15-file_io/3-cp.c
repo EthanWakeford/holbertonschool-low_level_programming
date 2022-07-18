30ef0c6065531a36357ba1a6dbd783cce9dc4248
EthanWakeford <ethanwakeford@gmail.com>
2022-07-18 13:40:54 -0700

    'added 3'

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+*main - copies file contents to another file
+*@argc: count of arguments
+*@argv: pointer to list of argument strings
+*Return: always 0
+*/
+
+int main(int argc, char **argv)
+{
+	ssize_t fd_1, fd_2, output;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if (argv[1] == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_1 = open(argv[1], O_RDONLY);
+	fd_2 = open(argv[2], O_CREAT | O_RDWR, 00664);
+	output = write(fd_2, buf, read(fd_1, buf, 1024));
+	if ((output == -1) | (fd_1 == -1))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (close(fd_1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd_1);
+		exit(100);
+	}
+	if (close(fd_2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd_2);
+		exit(100);
+	}
+	return (0);
+}
