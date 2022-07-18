a21937da7779c4c212bd3e61948312d232b858bd
EthanWakeford <ethanwakeford@gmail.com>
2022-07-18 11:46:59 -0700

    'added 0'

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,10 +9,25 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t n;
+	ssize_t n, fd, size;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
-	n = write(1, filename, letters);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		return (0);
+		free(buf);
+	}
+	size = read(fd, buf, letters);
+	if (size < 0)
+		return (0);
+	n = write(1, buf, size);
+	free(buf);
+	close(fd);
 	return (n);
 }
