303a9566103aa8d18d60dec265771d8eee7d195b
EthanWakeford <ethanwakeford@gmail.com>
2022-07-18 14:30:16 -0700

    'updated 3'

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,20 +14,13 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	/*
-*	if (argv[1] == NULL)
-*	{
-*		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-*		exit(98);
-*	}
-	*/
 	fd_1 = open(argv[1], O_RDONLY);
 	if (fd_1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 00664);
+	fd_2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 00645)i;
 	if (fd_2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	print_len = write(fd_2, buf, read(fd_1, buf, 1024));
