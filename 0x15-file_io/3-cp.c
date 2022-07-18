75860826c256af3f76140e489d581003657b5a9e
EthanWakeford <ethanwakeford@gmail.com>
2022-07-18 14:05:28 -0700

    'updated 3'

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,25 +30,13 @@ int main(int argc, char **argv)
 	}
 	fd_2 = open(argv[2], O_TRUNC | O_CREAT | O_RDWR, 00664);
 	if (fd_2 == -1)
-	{	
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	output = write(fd_2, buf, read(fd_1, buf, 1024));
 	if (output == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (close(fd_1) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd_1);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd_1), exit(100);
 	if (close(fd_2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd_2);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd_2), exit(100);
 	return (0);
 }
