ed9dc753cee1e148a61265b5710799c85e250662
EthanWakeford <ethanwakeford@gmail.com>
2022-07-18 12:43:07 -0700

    'updated 1'

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		write(fd, text_content, strlen(text_content) + 1);
+		write(fd, text_content, strlen(text_content));
 	}
 	close(fd);
 	return (1);
