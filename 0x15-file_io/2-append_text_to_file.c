f87fa2d9f1736a0572f184585d2250baa12f9136
EthanWakeford <ethanwakeford@gmail.com>
2022-07-18 12:47:47 -0700

    'updated 2'

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+*append_text_to_file - appends text to file
+*@filename: name of file
+*@text_content: text to add
+*Return: 1 on success, -1 on failure
+*/
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t fd;
