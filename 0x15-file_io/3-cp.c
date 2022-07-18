188922fcdce4ccfedd2b3313e66fb0ec16807b25
EthanWakeford <ethanwakeford@gmail.com>
2022-07-18 14:27:36 -0700

    'updated 3'

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,11 +14,13 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	if (argv[1] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	/*
+*	if (argv[1] == NULL)
+*	{
+*		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+*		exit(98);
+*	}
+	*/
 	fd_1 = open(argv[1], O_RDONLY);
 	if (fd_1 < 0)
 	{
