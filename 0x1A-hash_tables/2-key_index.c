0c62bbdd6f4450391679bb93131bc74fe3112233
EthanWakeford <ethanwakeford@gmail.com>
2022-08-03 10:51:54 -0700

    'added 2'

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+*key_index - returns the index of a key
+*@key: key
+*@size: size of hash table
+*Return: index
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash = hash_djb2(key);
+
+	hash = hash % size;
+	return (hash);
+}
