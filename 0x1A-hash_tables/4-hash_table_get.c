48a4dc4f34a0d454ec61d0b02420daad4d1be731
EthanWakeford <ethanwakeford@gmail.com>
2022-08-04 08:58:45 -0700

    'updated 4 function description'

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+*hash_table_get - returns the value at a given key
+*@ht: hash table
+*@key: the key
+*Return: value, or NULL if cannot be found
+*/
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = key_index((unsigned char *)key, ht->size);
