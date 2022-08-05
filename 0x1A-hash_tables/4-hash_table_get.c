a5779c8cd1d2273dc31d9241db5c66055915132f
EthanWakeford <ethanwakeford@gmail.com>
2022-08-05 09:07:38 -0700

    'uodated 4'

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,10 +9,11 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	unsigned long int index;
 
-	if (key == NULL)
+	if (key == NULL || ht == NULL)
 		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
 	return (ht->array[index]->value);
