e6829c5e9079c48a1b1f004af1c57964999505eb
EthanWakeford <ethanwakeford@gmail.com>
2022-08-05 07:20:50 -0700

    'updated 3'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,6 +13,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((unsigned char *)key, ht->size);
 	hash_node_t *new_node;
 
+	if (key == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
