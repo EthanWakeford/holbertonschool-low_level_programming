1c20510fba267db2976c32511bd9f5b91b5ae567
EthanWakeford <ethanwakeford@gmail.com>
2022-08-05 08:42:36 -0700

    'updated 3'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,6 +24,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		for (tmp = ht->array[index]; tmp != NULL; tmp = tmp->next)
 		{
+			if (strcmp(tmp->key, key) == 0)
+				free(tmp);
 		}
 		new_node->next = ht->array[index];
 	}
