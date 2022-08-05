7d386946143d67ce3e1c403da3287efaaff44ca4
EthanWakeford <ethanwakeford@gmail.com>
2022-08-05 09:13:43 -0700

    'updated 4'

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,11 +10,22 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *tmp;
 
 	if (key == NULL || ht == NULL)
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
+	tmp = ht->array[index];
+	if (tmp == NULL)
 		return (NULL);
+	if (tmp->next != NULL)
+	{
+		for (;tmp != NULL: tmp = tmp->next)
+		{
+			if (strcmp(tmp->key, key) == 0)
+				return (tmp->value);
+		}
+		return (NULL);
+	}
 	return (ht->array[index]->value);
 }
