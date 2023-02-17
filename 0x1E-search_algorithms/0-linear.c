f52d116d7aeeb72742eaf2964a0bae4ae480e090
EthanWakeford <ethanwakeford@gmail.com>
2023-02-17 13:08:27 -0600

    updated function description

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,7 +7,7 @@
  * @size: size of array
  * @value: value to search for
  *
- * return: index of value, or zero if array is NULL or value not present
+ * Return: index of value, or zero if array is NULL or value not present
 */
 
 int linear_search(int *array, size_t size, int value)
